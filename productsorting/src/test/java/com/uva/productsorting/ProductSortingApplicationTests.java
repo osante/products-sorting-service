7855463b564773ad7520af2e6c0cd8cbb54d03e5
Ulises Garcia Osante <ulises.osante@gmail.com>
2025-07-03 10:27:40 +0200

    Project template

---
+++ productsorting/src/test/java/com/uva/productsorting/ProductSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uva.productsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
